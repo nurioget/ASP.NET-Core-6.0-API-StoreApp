96d5f7c753c50c5bb070daf21ec891e2c77d7b3e
nurioget <nurioget9@gmail.com>
2024-05-03 10:00:08 +0300

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers()
+    .AddNewtonsoftJson();
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<RepositoryContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("sqlConnection"))
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ WebApi/Repositories/Config/BookConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApi.Models;
+
+namespace WebApi.Repositories.Config
+{
+    public class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.HasData(
+                new Book { Id = 1, Title = "suç ve ceza", Price = 30 },
+                new Book { Id = 2, Title = "mesneviden dersler", Price = 356 },
+                new Book { Id = 3, Title = "Devlet", Price = 42 }
+             );
+        }
+
+    }
+}
